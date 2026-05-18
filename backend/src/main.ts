dafc17480d8c95ea94f908f3a3d2509317a42142
STC 33 <anatolie.berco@gmail.com>
2026-05-18 12:01:42 +0300

    Fix CORS: allow all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://ioc-vault-frontend-spodh6aucq-ew.a.run.app',
-    ],
-  });
+  app.enableCors({ origin: true });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, transform: true }),
